509f37d6f39f53d8d2f9fe70e9376719c3f1e135
Kevin De Lama <74796011+KevinDeLama2020@users.noreply.github.com>
2021-05-25 17:12:58 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,40 @@
 #include <iostream>
+#include "bst.h"
+#include "type.h"
 
-// Min Heap: https://www.cs.usfca.edu/~galles/visualization/Heap.html
+
+using namespace std;
+//Trabajo realizado por Maizo Diego y De Lama Kevin
 
 int main() {
-  std::cout << "Hola!" << endl;
-  std::cout << "Puedes encontrar el ejemplo de heap en el siguiente enlace:\n https://www.cs.usfca.edu/~galles/visualization/Heap.html" << endl;
-  std::cout << "Se sugiere aplicar una reingeniería para poder hacer el max heap.";
+MinHeap<T1> z;
+MaxHeap<T1> x;
+ 
+
+/*T1 test =0;
+for(; test<10; test++)
+  z.push(test);*/
+z.push(29);
+z.push(10);
+z.push(3);
+z.push(1);
+z.push(2);
+z.push(12);
+z.push(67);
+z.push(76);
+z.print();
+
+cout<<"\n....."<<endl;
+x.push(29);
+x.push(10);
+x.push(3);
+x.push(1);
+x.push(2);
+x.push(12);
+x.push(67);
+x.push(76);
+x.print();
+
+
   return 0;
 }

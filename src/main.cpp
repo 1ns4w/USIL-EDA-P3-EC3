59e037dc20f2ba53a2135802007a2cf3802b2ee9
Joshua Christian Soria <74795522+josh18151@users.noreply.github.com>
2021-05-26 00:28:32 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,44 @@
 #include <iostream>
+#include <bits/stdc++.h>
+#includ <bst.h>
+using namespace std;
 
-// Min Heap: https://www.cs.usfca.edu/~galles/visualization/Heap.html
+//MinHeap implementacion
+int main()
+{
+  MinHeap h(9);
+  h.insertKey(1);
+  h.insertKey(7);
+  h.insertKey(3);
+  h.insertKey(34);
+  h.insertKey(74);
+  h.insertKey(22);
+  h.insertKey(10);
+  h.insertKey(33);
+  h.insertKey(15);
+  h.insertKey(11);
+  cout << h.extractMin() << " ";
+  cout << h.getMin() << " ";
+  h.decreaseKey(2, 1);
+  cout << h.getMin();
+  return 0;
+}
 
-int main() {
-  std::cout << "Hola!" << endl;
-  std::cout << "Puedes encontrar el ejemplo de heap en el siguiente enlace:\n https://www.cs.usfca.edu/~galles/visualization/Heap.html" << endl;
-  std::cout << "Se sugiere aplicar una reingeniería para poder hacer el max heap.";
+//Max Heap implementacion
+int main()
+{
+  priority_queue <int> pq;
+  pq.push(10);
+  pq.push(30);
+  pq.push(80);
+  pq.push(20);
+  pq.push(40);
+  
+  while (pq.empty() == false)
+  {
+    cout << pq.top() << " ";
+    pq.pop();
+  }
+  
   return 0;
 }
